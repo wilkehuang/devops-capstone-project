54f8ce0fe3c0bfb11780fe4b8a3fd343d4dcecaa
wilkehuang <wilkehuang@hotmail.com>
2022-12-23 11:29:40 -0500

    completed ci build 3

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
@@ -89,11 +90,10 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
 
     return account.serialize(), status.HTTP_200_OK
 
